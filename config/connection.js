93b4e0e6cf60ad4030c30bf5809adac82bfa1226
AngelDragoniusX <dialga1994@gmail.com>
2021-08-15 23:20:49 -0400

    added dependencies, seeds, js connection, sequelize, and updated README

--- config/connection.js
+++ config/connection.js
@@ -2,9 +2,21 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
+console.log("Env", process.env.DB_NAME);
+
+// const sequelize = process.env.JAWSDB_URL
+//   ? new Sequelize(process.env.JAWSDB_URL)
+//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//       host: 'localhost',
+//       dialect: 'mysql',
+//       dialectOptions: {
+//         decimalNumbers: true,
+//       },
+//     });
+
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  : new Sequelize("ecommerce_db", "root", "saber", {
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
